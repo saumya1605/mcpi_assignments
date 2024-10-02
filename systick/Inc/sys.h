c9975bcd67773a95d4535d6c4b7b93ade4cf2365
saumya1605 <saumyamishra0805@gmail.com>
2024-10-02 18:37:02 +0530

    systick

---
+++ systick/Inc/sys.h
@@ -0,0 +1,20 @@
+/*
+ * sys.h
+ *
+ *  Created on: Oct 2, 2024
+ *      Author: sunbeam
+ */
+
+#ifndef SYS_H_
+#define SYS_H_
+
+#include "stm32f4xx.h"
+
+void SysTick_Handler(void);
+void SysTick_Delay(uint32_t ms);
+
+
+
+
+
+#endif /* SYS_H_ */
